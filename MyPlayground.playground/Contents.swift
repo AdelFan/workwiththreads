25db44dee2f7808ff7cbdbb68d092bab8faa659e
Drdev <fallen.bmxer@gmail.com>
2022-02-10 15:37:14 +0500

    Add bug-fix.

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,6 +3,8 @@ import UIKit
 public var condition = NSCondition()
 public var isOpenForChange = false
 
+
+
 let storage = Storage()
 let generationThread = Generator(storage: storage)
 let workingThread = Worker(storage: storage)
